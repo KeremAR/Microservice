5e6d063d7c77d090c98c1372d82f25a4a62bf558
emirnar2701@gmail.com <emirnar2701@gmail.com>
2025-04-20 22:05:14 +0300

    department_service first draft

---
+++ department-service/src/main/java/com/example/departmanservice/DepartmanServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.departmanservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmanServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmanServiceApplication.class, args);
+	}
+
+}
---
+++ department-service/src/test/java/com/example/departmanservice/DepartmanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.departmanservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmanServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
